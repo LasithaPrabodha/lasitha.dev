1b2b87e5c6ef9d1c97ccdc54ee5950cd253a2a96
Lasitha Weligampola <lasiprabo@gmail.com>
2024-02-17 14:47:56 -0500

    astro version init

---
+++ src/consts.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = 'Lasitha Prabodha Weligampola';
+export const SHORT_SITE_TITLE = "LP."
+export const SITE_DESCRIPTION = 'Experienced full-stack web developer with expertise in building robust, scalable, and user-friendly applications. Skilled in modern Javascript UI frameworks such as React, Angular and VueJS, and proficient in object-oriented languages like C#, TypeScript and Java. Passionate about leveraging technology to deliver innovative solutions and improve user experiences. Connect with me to discuss collaborations and explore opportunities.';
