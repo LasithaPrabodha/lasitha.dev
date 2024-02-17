24a560954c27ab3e23c1f3288c6c4b9c80026d0c
Lasitha Weligampola <lasiprabo@gmail.com>
2024-02-17 16:48:56 -0500

    theme toggle icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  integrations: [preact()]
 });
---
+++ src/assets/js/night.js
@@ -0,0 +1,97 @@
+const backgroundColor = "#030318";
+const width = window.innerWidth;
+const height = window.innerHeight;
+const maxStarRadius = 1.5;
+const minStarOpacity = 0.1;
+const maxStarOpacity = 0.7;
+const stars = createStars(width, height, 50);
+const moon = {
+  color: "#fea",
+  x: width - width / 3.3,
+  y: height - height / 4,
+  r: 30,
+};
+
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = width;
+canvas.height = height;
+
+let counter = 0;
+
+function checkIfMobile() {
+  if ("maxTouchPoints" in navigator) {
+    return navigator.maxTouchPoints > 0;
+  } else if ("msMaxTouchPoints" in navigator) {
+    return navigator.msMaxTouchPoints > 0;
+  } else {
+    const mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+    if (mQ && mQ.media === "(pointer:coarse)") {
+      return !mQ.matches;
+    } else if ("orientation" in window) {
+      return true; // deprecated, but good fallback
+    } else {
+      // Only as a last resort, fall back to user agent sniffing
+      const UA = navigator.userAgent;
+      return /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) || /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+    }
+  }
+}
+
+function randomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function createStars(width, height, spacing) {
+  const stars = [];
+
+  for (let x = 0; x < width; x += spacing) {
+    for (let y = 0; y < height; y += spacing) {
+      const star = {
+        x: x + randomInt(spacing),
+        y: y + randomInt(spacing),
+        r: Math.random() * maxStarRadius,
+      };
+      stars.push(star);
+    }
+  }
+  return stars;
+}
+
+function fillCircle(ctx, x, y, r, fillStyle) {
+  ctx.beginPath();
+  ctx.fillStyle = fillStyle;
+  ctx.arc(x, y, r, 0, Math.PI * 2);
+  ctx.fill();
+}
+
+function renderMoon(ctx, blur) {
+  fillCircle(ctx, moon.x, moon.y, moon.r, moon.color);
+  // render a smaller circle above the moon to give it that well-known moon-shape
+  fillCircle(ctx, moon.x - moon.r / 3, moon.y - moon.r / 3, moon.r, backgroundColor);
+}
+
+function getOpacity(factor) {
+  const opacityIncrement = (maxStarOpacity - minStarOpacity) * Math.abs(Math.sin(factor));
+  const opacity = minStarOpacity + opacityIncrement;
+  return opacity;
+}
+
+export function render() {
+  ctx.fillStyle = backgroundColor;
+  ctx.fillRect(0, 0, width, height);
+  stars.forEach(function (star, i) {
+    const factor = counter * i;
+    const x = star.x;
+    const y = star.y;
+    const opacity = getOpacity(factor);
+    fillCircle(ctx, x, y, star.r, `rgba(255, 255, 255, ${opacity}`);
+  });
+
+  const isMobile = checkIfMobile();
+
+  !isMobile && renderMoon(ctx);
+
+  counter++;
+  requestAnimationFrame(render);
+}
