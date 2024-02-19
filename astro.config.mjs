b8da82136247b6ab473151e545a10d8c0a4370a5
Lasitha Weligampola <lasiprabo@gmail.com>
2024-02-19 18:24:33 -0500

    google tag manager

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,22 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify";
-
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   site: "https://lasitha.netlify.app",
-  integrations: [mdx(), sitemap(), tailwind()],
-  adapter: netlify()
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
+  adapter: netlify(),
 });
