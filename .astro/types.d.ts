c9e5f5cf001db470ee6e0102976f0ae1b5f121e0
Lasitha Weligampola <lasiprabo@gmail.com>
2024-02-19 13:18:11 -0500

    rect pattern in blogs page

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -136,37 +136,23 @@ declare module 'astro:content' {
 
 	type ContentEntryMap = {
 		"blog": {
-"first-post.md": {
-	id: "first-post.md";
-  slug: "first-post";
+"my-journey.mdx": {
+	id: "my-journey.mdx";
+  slug: "my-journey";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".mdx"] };
+"observer-pattern.md": {
+	id: "observer-pattern.md";
+  slug: "observer-pattern";
   body: string;
   collection: "blog";
   data: InferEntrySchema<"blog">
 } & { render(): Render[".md"] };
-"markdown-style-guide.md": {
-	id: "markdown-style-guide.md";
-  slug: "markdown-style-guide";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] };
-"second-post.md": {
-	id: "second-post.md";
-  slug: "second-post";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] };
-"third-post.md": {
-	id: "third-post.md";
-  slug: "third-post";
-  body: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] };
-"using-mdx.mdx": {
-	id: "using-mdx.mdx";
-  slug: "using-mdx";
+"singleton-pattern.mdx": {
+	id: "singleton-pattern.mdx";
+  slug: "singleton-pattern";
   body: string;
   collection: "blog";
   data: InferEntrySchema<"blog">
