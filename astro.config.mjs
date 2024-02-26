ee9f8552c62f2f84564ae2eaa1149911608c6174
Lasitha Weligampola <lasiprabo@gmail.com>
2024-02-25 22:43:11 -0500

    site name updated in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import partytown from "@astrojs/partytown";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  site: "https://lasitha.netlify.app",
+  site: "https://lasitha.dev",
   integrations: [
     mdx(),
     sitemap(),
