67666fab3b4784b1f88835a342696c8fbd1f30cc
Lasitha Weligampola <lasiprabo@gmail.com>
2024-03-01 22:23:45 -0500

    fix for banner

--- src/utils/openGraph.tsx
+++ src/utils/openGraph.tsx
@@ -1,26 +1,20 @@
-import sharp from "sharp";
+import { Resvg } from "@resvg/resvg-js";
 import { html } from "satori-html";
-import { readFile } from "node:fs/promises";
-import satori, { type SatoriOptions } from "satori";
+import satori from "satori";
 import { SITE_TITLE } from "../consts";
+import { readFile } from "node:fs/promises";
+
+import Poppins from "@fontsource/poppins/files/poppins-devanagari-600-normal.woff";
+
+const dimensions = {
+  width: 1200,
+  height: 630,
+};
 
 export interface OgData {
   title: string;
   date: Date;
 }
-
-/**
- * Opengraph template to generate svg
- */
-const Template = (props: OgData) => (
-    html` <div class="bg-zinc-900 p-26 flex flex-col w-full h-full items-center">
-    <img style="width: 20rem; border: 6px solid white;" src="https://lasitha.dev/static/casual-photo.jpg"  class="rounded-full" />
-
-    <div class="flex flex-col ml-4 mt-14">
-      <span style="" class="text-white font-bold text-4xl">Lasitha Prabodha Weligampola</span>
-    </div>
-  </div>`
-);
 
 /**
  * generate filename / path for generated OG images
@@ -43,40 +37,67 @@ export const getOgImagePath = (filename: string = SITE_TITLE) => {
  *
  * @param text
  */
-const generateOgImage = async (text: string = SITE_TITLE, date: Date = new Date()): Promise<Buffer> => {
-  const options: SatoriOptions = {
-    width: 1200,
-    height: 630,
-    embedFont: true,
+const generateOgImage = async (title: string = SITE_TITLE, date: Date = new Date()): Promise<Buffer> => {
+  const markup = html`<div class="bg-zinc-900 flex flex-col w-full h-full" style="font-family: 'Quicksand'">
+    <div class="flex flex-col w-full h-4/5 p-10 justify-center">
+      <div class="text-zinc-400 text-2xl mb-6">
+        ${date.toLocaleDateString("en-us", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })}
+      </div>
+      <div
+        class="flex text-6xl w-full font-bold leading-snug tracking-tight text-transparent bg-red-400"
+        style="font-family: Poppins, sans-serif;
+      -webkit-text-fill-color: transparent;
+      background-clip: text;
+      -webkit-background-clip: text;
+      background-image: linear-gradient(45deg, #90a0d9, #eee);"
+      >
+        ${title}
+      </div>
+    </div>
+    <div class="w-full h-1/5 border-t border-zinc-700/50 flex p-10 items-center justify-between text-2xl">
+      <div class="flex items-center">
+        <span class="ml-3 text-zinc-400">lasitha.dev</span>
+      </div>
+      <div class="flex items-center">
+        <img src="https://lasitha.dev/static/casual-photo.jpg" class="w-20 h-20 rounded-full" />
+        <div class="flex flex-col ml-4">
+          <span class="text-zinc-400">Lasitha Weligampola</span>
+          <span class="text-blue-400">@lu6_fer</span>
+        </div>
+      </div>
+    </div>
+  </div>`;
+
+  
+  const svg = await satori(markup, {
     fonts: [
       {
-        name: "Poppins",
-        data: await readFile("./fonts/Poppins-Regular.ttf"),
-        weight: 400,
-        style: "normal",
+        name: "Quicksand",
+        data:  await readFile("./dist/fonts/Quicksand-SemiBold.ttf"),
+        weight: 500,
       },
       {
-        name: "PoppinsSemiBold",
-        data: await readFile("./fonts/Poppins-SemiBold.ttf"),
-        weight: 600,
-        style: "normal",
-      },
+        name: "Poppins",
+        data:  await readFile("./dist/fonts/Poppins-SemiBold.ttf"),
+        weight: 700,
+      }
     ],
-  };
+    width: dimensions.width,
+    height: dimensions.height,
+  });
 
-  const svg = await satori(
-    Template({
-      title: text,
-      date: date,
-    }),
-    options
-  );
+  const image = new Resvg(svg, {
+    fitTo: {
+      mode: "width",
+      value: dimensions.width,
+    },
+  }).render();
 
-  const sharpSvg = Buffer.from(svg);
-
-  const buffer = await sharp(sharpSvg).toBuffer();
-
-  return buffer;
+  return image.asPng();
 };
 
 export default generateOgImage;
