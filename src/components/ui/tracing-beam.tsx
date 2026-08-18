7c976e897e8a9e5d54ec9214f4998c466be5559e
Lasitha Weligampola <lasiprabo@gmail.com>
2026-08-18 19:40:55 -0400

    no dot on top of the tracing beam

--- src/components/ui/tracing-beam.tsx
+++ src/components/ui/tracing-beam.tsx
@@ -51,32 +51,8 @@ export const TracingBeam = ({
       ref={ref}
       className={cn("relative mx-auto h-full w-full max-w-4xl", className)}
     >
-      <div className="absolute top-10 -left-4 md:-left-24 hidden md:block">
-        <motion.div
-          transition={{
-            duration: 0.2,
-            delay: 0.5,
-          }}
-          animate={{
-            boxShadow:
-              scrollYProgress.get() > 0
-                ? "none"
-                : "rgba(0, 0, 0, 0.24) 0px 3px 8px",
-          }}
-          className="border-netural-200 ml-[27px] flex h-4 w-4 items-center justify-center rounded-full border shadow-sm"
-        >
-          <motion.div
-            transition={{
-              duration: 0.2,
-              delay: 0.5,
-            }}
-            animate={{
-              backgroundColor: scrollYProgress.get() > 0 ? "white" : "#10b981",
-              borderColor: scrollYProgress.get() > 0 ? "white" : "#059669",
-            }}
-            className="h-2 w-2 rounded-full border border-neutral-300 bg-white"
-          />
-        </motion.div>
+      <div className="absolute top-10 -left-4 md:-left-48 hidden md:block">
+          
         <svg
           viewBox={`0 0 20 ${svgHeight}`}
           width="20"
